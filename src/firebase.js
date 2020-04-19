89634466c908bcdee8dd42a353a1fb09a5efba51
Jorge Contreras <jorge.contreras789@gmail.com>
2020-04-19 09:29:41 -0400

    Added OAuth

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAIKzUNQZoqqyN7MLDZx-ZJkccOhFFi4LY',
@@ -16,6 +17,11 @@ firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
 
 export const firestore = firebase.firestore();
+export const auth = firebase.auth();
+
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const signOut = () => auth.signOut();
 
 // ! Remove from production
 window.firebase = firebase;
